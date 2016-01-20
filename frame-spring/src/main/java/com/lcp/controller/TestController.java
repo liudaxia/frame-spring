09d1ad2d6da25fccec96444fd01688751a394235
liudaxia <476578985@qq.com>
2016-01-20 15:42:55 +0800

    add spring project
    a simple ,pure spring framework

---
+++ frame-spring/src/main/java/com/lcp/controller/TestController.java
@@ -0,0 +1,24 @@
+package com.lcp.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.lcp.service.TestService;
+
+@Controller
+@RequestMapping(value="/test")
+public class TestController {
+	@Autowired
+	private TestService testService;
+	
+	@RequestMapping(value="/testName.do")
+	public void testName(HttpServletRequest req,HttpServletResponse resp,String name){
+		System.out.println("TestController testName params:"+name);
+		String word=testService.TestName(name);
+		System.out.println("TestController testName returns:"+word);
+	};
+}
---
+++ frame-spring/src/main/webapp/index.jsp
@@ -0,0 +1,9 @@
+<html>
+<body>
+<form action="test/testName.do" >
+<input type="text" name="name">
+<input type="submit" value="haha">
+</form>
+<h2>Hello World!</h2>
+</body>
+</html>
